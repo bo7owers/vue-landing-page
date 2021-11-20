5f55fa9414ae3177703a1ece8da3d8f73603b00f
René Torres <7orresrene@users.noreply.github.com>
2021-11-19 23:01:04 -0500

    Add a page to the router, play with nav

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
-
-  <router-view/>
+  <div class="nav-flex">
+    <router-link to="/">Home</router-link>
+    <router-link to="/Bienvenue">Bienvenue</router-link>
+  </div>
+  <router-view />
 </template>
 
 <style lang="scss">
- @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital@0;1&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:ital@0;1&display=swap");
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-      font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
 }
 
+.nav-flex {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+  background: #369;
+  a {
+    color: #f3c2f5;
+    text-decoration: none;
+    transition: all 7s ease-in-out color 0.5s;
+    &:visited {
+      color: #f3c2f5;
+    }
+    &:hover {
+      text-decoration: underline;
+      color: #e0ffb8;
+      scale: 1.1;
+    }
+  }
+}
 </style>
---
+++ src/views/Bienvenue.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="welcome">Bienvenue, welcomes you to this new experience</div>
+</template>
+
+<script>
+export default {
+  name: "Bienvenue",
+};
+</script>
+
+<style lang="scss" scoped>
+#welcome {
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(50%, 50%);
+}
+</style>
