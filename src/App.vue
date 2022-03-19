f90944d49dcc5a16d4d014657d6f1e80b36c8a77
René Torres <7orresrene@users.noreply.github.com>
2022-03-19 13:42:10 -0400

    Install primevue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-
-  <router-view/>
+	<router-view />
 </template>
 
 <style lang="scss">
- @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital@0;1&display=swap');
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-      font-family: 'Montserrat', sans-serif;
-}
-
+	@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital@0;1&display=swap');
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+		font-family: 'Montserrat', sans-serif;
+	}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,47 +1,56 @@
 <template>
-  <div class="home">
-    <Modal @close="toggleModal" :modalActive="modalActive">
-      <div class="modal-content">
-        <div class="modal-title">
-        <h1>
-          This is the title
-        </h1>
-        </div>
-        <div class="modal-body">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati adipisci similique ipsam qui fugiat? Dolorum necessitatibus voluptates aspernatur repellendus adipisci nulla fugit quidem quae magni.</p>
-        </div>
-      </div>
-    </Modal>
-    <button @click="toggleModal" type="button" class="point-modal open-modal">Open Modal</button>
-  </div>
+	<div class="home">
+		<Modal @close="toggleModal" :modalActive="modalActive">
+			<div class="modal-content">
+				<div class="modal-title">
+					<h1>This is the title</h1>
+				</div>
+				<div class="modal-body">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing
+						elit. Obcaecati adipisci similique ipsam qui fugiat?
+						Dolorum necessitatibus voluptates aspernatur repellendus
+						adipisci nulla fugit quidem quae magni.
+					</p>
+				</div>
+			</div>
+		</Modal>
+		<div class="btn">
+			<Button
+				@click="toggleModal"
+				type="button"
+				class="point-modal open-modal"
+				label="Open Modal"
+			></Button>
+		</div>
+	</div>
 </template>
 
 <script>
-import Modal from '../components/Modal.vue'
-import { ref } from 'vue'
-export default {
-  name: 'Home',
-  components: {
-    Modal
-  },
-  setup () {
-    const modalActive = ref(false)
-    const toggleModal = () => {
-        modalActive.value = !modalActive.value
-}
+	import Modal from '../components/Modal.vue'
+	import { ref } from 'vue'
+	export default {
+		name: 'Home',
+		components: {
+			Modal,
+		},
+		setup() {
+			const modalActive = ref(false)
+			const toggleModal = () => {
+				modalActive.value = !modalActive.value
+			}
 
-
-    return { modalActive, toggleModal }
-  }
-}
+			return { modalActive, toggleModal }
+		},
+	}
 </script>
 
 <style lang="scss" scoped>
-.home {
-  background: #222;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+	.home {
+		background: #222;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 </style>
